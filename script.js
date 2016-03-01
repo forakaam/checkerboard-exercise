502ff9bc43fdc4b14920e5dbb4eda5c8af027d23
Forrest Ambruster <forrest_etc@earthlink.net>
2016-02-29 18:03:54 -0800

    gradients are really hard

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 // Your JS goes here
 body = document.querySelector("body");
 var tiles = [];
-function randomColor (){
-  var RGBArray = [];
-  for (var i =0; i < 3; i ++) {
-    RGBArray.push(Math.floor(Math.random() * 255));
-  }
-  return "rgb(" + RGBArray[0] + "," + RGBArray[1] + "," + RGBArray[2] + ")" ;
-}
+var firstNum = 10;
+var secondNum = 0;
+var thirdNum = 255;
 
+function gradient1 () {
+  thirdNum -=10; 
+  secondNum +=10;
+ // if (secondNum % 2 ===0){
+  //  firstNum +=10;
+ // }
+  while (thirdNum < 0) {
+    thirdNum +=50;
+  }
+  while (secondNum > 255){
+    secondNum  -=50;
+  }
+ while (firstNum > 255){
+    firstNum -=25;
+ }
+  return "rgb(" + firstNum + "," + secondNum + "," + thirdNum + ")" ;
+}
 function tiler (num) {
-  var count = 1;
   for( var i =0; i < num; i++){
     tiles.push(document.createElement("div"));
     body.appendChild(tiles[i]);
     tiles[i].style.width = "11.1%";
     tiles[i].style.float = "left";
-    tiles[i].style.paddingBottom = "11.1%";   
-    tiles[i].style.backgroundColor = randomColor();
-  }
-  count++; 
+    tiles[i].style.paddingBottom = "11.1%";
+    tiles[i].style.backgroundColor = gradient1();
+  } 
 }
 tiler(63);
