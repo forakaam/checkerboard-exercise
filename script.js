bc7189451b9cd046dba07aa4986481b89e1e976c
Forrest Ambruster <forrest_etc@earthlink.net>
2016-02-29 16:56:55 -0800

    basic checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+body = document.querySelector("body");
+var tiles = [];
+function tiler (num) {
+  var count = 1;
+  for( var i =0; i < num; i++){
+    tiles.push(document.createElement("div"));
+    body.appendChild(tiles[i]);
+    tiles[i].style.width = "11.1%";
+    tiles[i].style.float = "left";
+    tiles[i].style.paddingBottom = "11.1%";
+   if (count % 2 === 0) {
+     tiles[i].style.backgroundColor = "red";
+   }
+   else {
+    tiles[i].style.backgroundColor = "black";
+   }
+   count++; 
+  }
+}
+tiler(63);
