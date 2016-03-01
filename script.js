10feff60ce23ff752631a6b8162ea061fe562081
Forrest Ambruster <forrest_etc@earthlink.net>
2016-02-29 17:27:20 -0800

    random checkerboard

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Your JS goes here
 body = document.querySelector("body");
 var tiles = [];
+function randomColor (){
+  var RGBArray = [];
+  for (var i =0; i < 3; i ++) {
+    RGBArray.push(Math.floor(Math.random() * 255));
+  }
+  return "rgb(" + RGBArray[0] + "," + RGBArray[1] + "," + RGBArray[2] + ")" ;
+}
+
 function tiler (num) {
   var count = 1;
   for( var i =0; i < num; i++){
@@ -8,14 +16,9 @@ function tiler (num) {
     body.appendChild(tiles[i]);
     tiles[i].style.width = "11.1%";
     tiles[i].style.float = "left";
-    tiles[i].style.paddingBottom = "11.1%";
-   if (count % 2 === 0) {
-     tiles[i].style.backgroundColor = "red";
-   }
-   else {
-    tiles[i].style.backgroundColor = "black";
-   }
-   count++; 
+    tiles[i].style.paddingBottom = "11.1%";   
+    tiles[i].style.backgroundColor = randomColor();
   }
+  count++; 
 }
 tiler(63);
